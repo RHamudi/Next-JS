465d7c52c1e6c092408ec557955a1d026542c44c
RHamudi <ramonramos.silva16@gmail.com>
2022-10-05 18:18:38 -0300

    .

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,11 +1,33 @@
+import axios from "axios";
 import type { NextPage } from "next";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ repositories, date }: any) => {
   return (
     <div>
-      <h1>Hello Word Next-JS</h1>
+      <h1>{date}</h1>
+      <ul>
+        {repositories.map((repo: any, index: any) => (
+          <li key={index}>{repo.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
+
+export async function getStaticProps() {
+  const res = (await axios.get("https://api.github.com/users/RHamudi/repos"))
+    .data;
+
+  const data = res.map((repo: any) => repo);
+
+  return {
+    props: {
+      repositories: data,
+      date: new Date().toISOString(),
+    },
+    revalidate: 10,
+  };
+}
 
 export default Home;
